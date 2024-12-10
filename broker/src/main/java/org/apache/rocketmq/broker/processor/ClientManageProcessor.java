7f9e542c38fd9c7cfe1f28406026436d3a25a122
guyinyou <36399867+guyinyou@users.noreply.github.com>
2024-12-10 20:05:44 +0800

    [ISSUE #8988] Support dispatchBehindMilliseconds (#8989)
    * support dispatchBehindMilliseconds
    
    * Modify the initial value of currentReputTimestamp
    
    ---------
    
    Co-authored-by: guyinyou <guyinyou.gyy@alibaba-inc.com>

--- broker/src/main/java/org/apache/rocketmq/broker/processor/ClientManageProcessor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/processor/ClientManageProcessor.java
@@ -242,8 +242,7 @@ public class ClientManageProcessor implements NettyRequestProcessor {
         return response;
     }
 
-    public RemotingCommand checkClientConfig(ChannelHandlerContext ctx, RemotingCommand request)
-        throws RemotingCommandException {
+    public RemotingCommand checkClientConfig(ChannelHandlerContext ctx, RemotingCommand request) {
         final RemotingCommand response = RemotingCommand.createResponseCommand(null);
 
         CheckClientRequestBody requestBody = CheckClientRequestBody.decode(request.getBody(),
---
+++ example/src/main/java/org/apache/rocketmq/example/simple/PopPushConsumer.java
@@ -0,0 +1,49 @@
+package org.apache.rocketmq.example.simple;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageRequestMode;
+import org.apache.rocketmq.remoting.protocol.body.ClusterInfo;
+import org.apache.rocketmq.remoting.protocol.route.BrokerData;
+import org.apache.rocketmq.tools.admin.DefaultMQAdminExt;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+// PopPushConsumer.java
+public class PopPushConsumer {
+
+    public static final String CONSUMER_GROUP = "CID_JODIE_1";
+    public static final String TOPIC = "TopicTest";
+
+    // Or use AdminTools directly: mqadmin setConsumeMode -c cluster -t topic -g group -m POP -n 8
+    private static void switchPop() throws Exception {
+        DefaultMQAdminExt mqAdminExt;
+        mqAdminExt = new DefaultMQAdminExt();
+        mqAdminExt.start();
+
+        ClusterInfo clusterInfo = mqAdminExt.examineBrokerClusterInfo();
+        Set<String> brokerAddrs = clusterInfo.getBrokerAddrTable().values().stream().map(BrokerData::selectBrokerAddr).collect(Collectors.toSet());
+
+        for (String brokerAddr : brokerAddrs) {
+            mqAdminExt.setMessageRequestMode(brokerAddr, TOPIC, CONSUMER_GROUP, MessageRequestMode.POP, 8, 3_000);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        switchPop();
+
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
+        consumer.subscribe(TOPIC, "*");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        consumer.setClientRebalance(false);
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
